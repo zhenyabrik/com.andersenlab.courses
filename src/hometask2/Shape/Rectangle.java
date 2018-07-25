15df1ed087ae364ca7aca33d37f90dcbef4d31a0
zhenyabrik <zhenyabrik2014@gmail.com>
2018-07-25 16:48:15 +0300

    fix hometask2

--- src/hometask2/Shape/Rectangle.java
+++ src/hometask2/Shape/Rectangle.java
@@ -6,18 +6,20 @@ public class Rectangle extends AbstractShape {
     private double rectangleWidth = randomGeometricDouble;
 
     @Override
-    public void calculateArea() {
+    public double calculateArea() {
         System.out.println("Rectangle height = " + rectangleHeight + " cm.");
         System.out.println("Rectangle width = " + rectangleWidth + " cm.");
         double tempArea;
         tempArea = rectangleHeight * rectangleWidth;
         area = Math.round(tempArea * 100.0) / 100.0;
         System.out.println("Rectangle area = " + area + " cm.2");
+        return area;
     }
 
     @Override
-    public void calculatePerimeter() {
+    public double calculatePerimeter() {
         perimeter = (2 * rectangleHeight) + (2 * rectangleWidth);
         System.out.println("Rectangle perimeter = " + perimeter + " cm.");
+        return perimeter;
     }
 }
